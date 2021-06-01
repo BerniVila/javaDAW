441e9aacd349f28f4c4feca766b1c1ea76498ef3
BerniVila <bernivila.web@gmail.com>
2021-06-01 10:13:22 +0200

    Empezando Proyecto de Fin de curso 1ºDAW

---
+++ DawProjectBerni/src/addNumbers/AddNumbersMainWindow.java
@@ -0,0 +1,43 @@
+package addNumbers;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AddNumbersMainWindow {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AddNumbersMainWindow window = new AddNumbersMainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AddNumbersMainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
